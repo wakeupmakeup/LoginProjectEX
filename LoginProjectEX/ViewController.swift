5a05e2b903a2306a6f4ff3af894a21d6b8d0cea5
wakeupmakeup <zerostar998@naver.com>
2022-11-30 01:18:43 +0900

    세번째 커밋 - isActive 한꺼번에 처리하는 법, 레이아웃 올리는 법, 스택뷰 코드로 짜는법,

--- LoginProjectEX/ViewController.swift
+++ LoginProjectEX/ViewController.swift
@@ -9,62 +9,169 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var emailTextFieldView : UIView = {
+    // MARK: - 이메일(아이디) 입력하는 텍스트 뷰
+    private lazy var emailTextFieldView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.darkGray
-        // 모서리 둥글게 처리하는 방법.
+        view.backgroundColor = .darkGray
         view.layer.cornerRadius = 12
-        view.layer.masksToBounds = true
-        view.addSubview(emailTextFieldView)  // 그냥 ViewDidLoad에 올려놓으면 인식이 되지 않는다. 따라서 이를 해결할 수 있는 메서드는 addSubview라는 것.
-                                             // 스토리보드에서 끌어다 놓으면 되는 부분이 바로 위에 부분이다.
+        view.clipsToBounds = true
+        view.addSubview(emailTextFieldView)
+        view.addSubview(emailInfoLabel)
+        /// addSubview를 사용하는 순서도 바뀌면 절대 안된다. 치즈 위에 햄, 햄 위에 치즈 같은 개념이다.
         return view
-    }()  // 뷰를 메모리에 올리는 활동.
-    /// 위에 해당 방식은 클로저 방식이다. 이렇게 하면 코드를 정리 할 수 있다. 내부에서 정의한 것을 emailTextFieldView에 저장하는 것이다.
-    /// 이 방식으로 오토레이아웃을 설정할 수는 없다. 왜냐면 오토레이아웃 부분이 더 나중에 실행될 부분이기 때문이다.
-    /// 하지만 view를 올리는 것은 가능하다. (addSubview) 그것은 바로 lazy var로 선언을 하면 된다. lazy var는 미리 메모리에 올라가 있는 것이 아닌 실행이 될때 올라가기 때문에 가능하다.
-    /// 먼저 ViewController 가 생성이 될때 view가 먼저 생성이 되야한다. 그렇기 때문에 view가 생성되고 나중에 lazy var 부분인 emailTextFieldView가 생성이 되니까 이때는 addSubview를 생성할 수 있다. 이 부분은 정답은 아니며 선호도에 따라 차이가 난다. 
+    }()
     
-
+    // 이메일 주소 안내 문구
+    private var emailInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이메일 주소"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return label
+    }()
+    
+    // login
+    private lazy var emailTextField: UITextField = {
+        var tf = UITextField()
+        
+        tf.frame.size.height = 18
+        tf.backgroundColor = .clear
+        tf.textColor = .white
+        tf.tintColor = .white
+        tf.autocapitalizationType = .none  // 자동으로 첫 글자를 대문자로 바꿔주는 것
+        tf.autocorrectionType = .no        // 자동으로 글자 타입 잡아주는것
+        tf.spellCheckingType = .no         // 오타 체크
+        tf.keyboardType = .emailAddress
+        
+        return tf
+    }()
     
     
+    // MARK: - 비밀번호 입력 필드
     
-
+    private lazy var passwordTextFieldView: UIView = {
+        let view = UIView()
+//        view.frame.size.height = 60
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    
+    // password 안내 문구
+    private var passwordInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .white
+        return label
+    }()
+    
+    // password TextField
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        
+        tf.backgroundColor = .darkGray
+        tf.frame.size.height = 60
+        tf.backgroundColor = .clear
+        tf.textColor = .white
+        tf.tintColor = .white
+        tf.autocapitalizationType = .none
+        tf.autocorrectionType = .no
+        tf.spellCheckingType = .no
+        tf.isSecureTextEntry = true  // 비밀번호 입력을 가리는 처리
+        tf.clearsOnBeginEditing = false
+        return tf
+    }()
+    
+    
+    // 비밀번호에 "표시" 버튼으로 가리기 활성/비활성화 만들기.
+    
+    private let passwordSecureButton: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setTitle("표시", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        return btn
+    }()
+    
+    //MARK: - 로그인버튼
+    
+    private let loginButton: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.backgroundColor = .clear
+        btn.layer.cornerRadius = 27.5
+        btn.clipsToBounds = true // 어떤 영역 내에 잘라낸다 라는 의미의 설정이며 코너 잘라내기 처리를 할때 이렇게 세트로 사용된다.
+        btn.layer.borderWidth = 0
+        btn.setTitle("로그인", for: .normal)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        btn.isEnabled = false  // 처음에 버튼이 비활성화 하게 만들기.
+        return btn
+    }()
+    
+    // 스택 뷰 만들기.
+    lazy var stackView: UIStackView = {
+//        let st = UIStackView()  -> 이렇게 설정한후 makeUI()에 등록한다.
+        
+        let st = UIStackView(arrangedSubviews: [emailTextFieldView, passwordTextField])
+        st.spacing = 16 // 스택 간의 내부 간격
+        st.axis = .vertical  // 묶을 축 설정
+        st.distribution = .fillEqually  // 간격을 어떻게 분배할 것인지?
+        st.alignment = .fill // 좌,우,가운데 정렬 선택
+        return st
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-     
-
+        
+        
         makeUI()
     }
     
-    func makeUI() { //
+    func makeUI() {
+        view.addSubview(emailTextFieldView)
+        /// 가장 아래에 있는 view에 emailTextFieldView를 올리고 또 거기에다 emailInfoLabel과 emailTextField를 올렸다.
         
-        // 코드로 ui를 짠다면 무조건 이 설정을 처음에 해줘야 한다. 안그럼 오류난다.  
-        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
-        /// 자동으로 설정되는 오토레이아웃을 끄는 것이다. 왜냐면 수동으로 하나하나 잡아줄 것이기 때문에 꺼야 크랙이 발생하지 않는다.
+//        stackView.addSubview(emailTextfieldView)
+//        stackView.addSubview(passwordTextField)
+//      이렇게 접근해서 스택뷰에 올려도 상관은 없다. 하지만 더 편하게 할 수 있으니 이렇게는 난 사용 x
         
         
-        //오토 레이아웃 잡기.
-        //스토리보드에선 높이 넓이 왼쪽 오른쪽 위 아래 다 잡아줬다. 이것을 코드로 하는 것일 뿐임.
+        
+        
 
-        // 왼쪽에서 얼마를 띄울 것인지 (leadingAnchor), constraint -> 조건, "얼마나"를 정하는 메서드, equalTo - "어디에다"부분
-        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
-        /// view.leadingAnchor -> 기본적인 view에서 앞쪽에다 맞추겠다. 라는 의미 그리고 constant는 얼마나 띄울 것인지 정하는 것임. 알기쉽게 한마디로 정리 하자면
-        /// emailTextFieldView를 왼쪽에서 띄울 것인데 얼마만큼 띄울 것이냐면, 왼쪽 view 끝을 기준으로 24만큼 띄울 것이고 이 설정을 동작(.isActive)하게 할 것이다.
-
-        // 오른쪽에서 얼마를 띄우 것인지 (trailingAnchor)
-        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
-
-        // 높이에서 얼마를 띄울 것인지(topAnchor)
-        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 313).isActive = true
+        // 오토레이아웃 설정.
+        // 끝에 isActive = true 쓰기가 엄청 귀찮은데 한꺼번에 처리할때 쓰는 것.
+        
+        emailInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            emailInfoLabel.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8),
+            emailInfoLabel.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: 8),
+            emailInfoLabel.centerYAnchor.constraint(equalTo: emailTextFieldView.centerYAnchor)
+            /// 항상 잊지 말자. 이렇게 오토레이아웃 설정을 했다면 무조건 뷰를 등록해야한다. addSubview
+        ])
         
         
-        // 높이 자체를 설정하는 것
-        // 높이는 기준이 없기 때문에 아래 처럼 설정하면 된다.
-        emailTextFieldView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+//        emailInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+//        emailInfoLabel.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8).isActive = true
+//        emailInfoLabel.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: 8).isActive = true
+//        emailInfoLabel.centerYAnchor.constraint(equalTo: emailTextFieldView.centerYAnchor).isActive = true
+//        /// 항상 잊지 말자. 이렇게 오토레이아웃 설정을 했다면 무조건 뷰를 등록해야한다. addSubview
         
+        
+        
+        // 이메일 텍스트 필드 레이아웃 설정.
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            emailTextField.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8),// 왼
+            emailTextField.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: 8), // 오
+            emailTextField.topAnchor.constraint(equalTo: emailTextFieldView.topAnchor, constant: 15), // 위
+            emailTextField.bottomAnchor.constraint(equalTo: emailTextFieldView.bottomAnchor, constant: 2) // 아래
+        ])
     }
-
-
 }
-
